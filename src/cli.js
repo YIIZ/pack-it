0419a2c27bf97196bc6542d79ced8877881afad7
anonymous <anonymous@users.noreply.com>
2020-04-09 10:41:34 +0800

    feat: add positional arguments

--- src/cli.js
+++ src/cli.js
@@ -5,42 +5,61 @@
 const yargs = require('yargs');
 const packIt = require('.');
 
-const argv = yargs
-  .usage('Usage: $0 -n [name] -t [type] -s [source dir] -o [output dir]')
-  .option('n', {
-    demandOption: true,
-    describe: "project's name",
+yargs
+  .command(
+    '$0 <dir> [options]',
+    'Pack files in directory',
+    (yargs) => {
+      yargs.positional('dir', {
+        description: 'directory to be packed',
+        type: 'string',
+      });
+    },
+    (argv) => {
+      const sourceDir = argv.dir;
+
+      const outputDir = argv.o;
+      const addTimestamp = argv.x;
+      const name = argv.n;
+      const type = argv.t;
+      const ignores = argv.i;
+
+      packIt(sourceDir, { outputDir, name, type, addTimestamp, ignores });
+    }
+  )
+  .option('name', {
+    alias: 'n',
+    demandOption: false,
+    default: 'archive',
+    describe: 'archive name',
     type: 'string',
   })
-  .option('t', {
+  .option('type', {
+    alias: 't',
     demandOption: true,
     default: 'ieg',
     choices: ['ieg'],
-    describe: "project's type",
+    describe: 'archive type',
     type: 'string',
   })
-  .option('s', {
-    demandOption: true,
-    describe: 'source directory to be packed ',
-    type: 'string',
-  })
-  .option('o', {
+  .option('output-dir', {
+    alias: 'o',
     demandOption: false,
     default: process.cwd(),
     describe: 'output directory which stores archives',
     type: 'string',
   })
-  .option('i', {
+  .option('add-timestamp', {
+    alias: 'x',
     demandOption: false,
-    describe: 'ignore file or directory to cdn',
+    default: false,
+    describe: "add timestamp to archive's name",
+    type: 'boolean',
+  })
+  .option('ignore-pattern', {
+    alias: 'i',
+    demandOption: false,
+    describe: 'ignore file or directory to CDN',
     type: 'array',
   })
   .help('h').argv;
-
-const name = argv.n;
-const type = argv.t;
-const sourceDir = argv.s;
-const outputDir = argv.o;
-const ignores = argv.i;
-
-packIt(name, type, sourceDir, outputDir, ignores);
